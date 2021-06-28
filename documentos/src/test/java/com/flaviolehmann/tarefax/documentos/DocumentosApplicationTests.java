7a2ea17f735ad852b327d6f655a23289e997cfa4
Flavio Lehmann <flavio.junior@basis.com.br>
2021-06-28 19:00:12 -0300

    front, service e documentos

---
+++ documentos/src/test/java/com/flaviolehmann/tarefax/documentos/DocumentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flaviolehmann.tarefax.documentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
